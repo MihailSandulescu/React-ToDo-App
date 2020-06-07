a52bd8751443e1b2195fdb66a20f8839a3e99d37
Mihai <sandulescu1mihail@gmail.com>
2020-06-07 15:10:45 +0300

    Updates

---
+++ precache-manifest.43de334c22b91cee9566024720c921aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3829752d1a5c0e26bfe45081d90bf31",
+    "url": "/React-ToDo-App/index.html"
+  },
+  {
+    "revision": "45ec595b8fe14a79c2de",
+    "url": "/React-ToDo-App/static/css/main.33eb0b94.chunk.css"
+  },
+  {
+    "revision": "00e89dcaec7b21645ab2",
+    "url": "/React-ToDo-App/static/js/2.359b839d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ToDo-App/static/js/2.359b839d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45ec595b8fe14a79c2de",
+    "url": "/React-ToDo-App/static/js/main.18b6d28c.chunk.js"
+  },
+  {
+    "revision": "2d9ead9aedbbfb0c2679",
+    "url": "/React-ToDo-App/static/js/runtime-main.dce107ec.js"
+  }
+]);
